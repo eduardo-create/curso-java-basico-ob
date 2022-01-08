c5dd23eb7a8c7ece133b56db9f8d094cd686ad98
Eduardo Neira <eduardo.neira31@gmail.com>
2022-01-08 18:07:31 -0300

    Tarea sesion 3

---
+++ src/com/openB/Sesion3.java
@@ -0,0 +1,12 @@
+package com.openB;
+
+public class Sesion3 {
+
+    public static void main(String[] args){
+        String[] dias = {"Lunes", "Martes", "Miércoles", "Jueves", "Viernes"};
+
+        for (String dia : dias) {
+            System.out.println(dia + ",");
+        }
+    }
+}
